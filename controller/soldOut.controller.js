c7dc44243b24bee9ddd57a45cc8c23a193fae2da
Surface1989 <ttmd210899@gmail.com>
2020-08-18 10:19:50 +0700

    change cart

--- controller/soldOut.controller.js
+++ controller/soldOut.controller.js
@@ -15,9 +15,21 @@ module.exports.soldOut = (req, res)=>{
     ]
 
     let totalCart = db.get('session').find({id: res.locals.sessionId}).get('total').value();
+    const width = 4;
+    const countPages = products.length / width + (products.length % width !== 0? 1: 0);
+    let start = 1;
+    let p = 1;
+    if(req.query.p){
+      p = req.query.p;
+      start = (p-1) * width;
+    }
+    let end = (start + width -1) >  products.length? products.length:  (start + width -1) ;
+    products = products.slice(start, end+1);
     res.render('sold-out', {    urls: urls,
                                 isActive: 'soldout',
                                 products: products,
-                                totalCart: totalCart 
+                                totalCart: totalCart, 
+                                countPages: countPages,
+                                active: parseInt(p) 
                               });
     }
